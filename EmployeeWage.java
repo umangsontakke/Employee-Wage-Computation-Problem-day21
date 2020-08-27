5372f94ee0b53c0ab723a55ba2429afed94eea10
umangsontakke <umangsontakke70@gmail.com>
2020-08-27 22:24:08 +0530

    TotalWorkHrs file added to uc6 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,38 +1,59 @@
-public class EmpWageMonth
-{
-	//instance variable
-public static final int IS_PART_TIME=1;
-public static final int IS_FULL_TIME=2;
-public static final int EMP_RATE_PER_HOUR=20;
-public static final int NUM_OF_WORKING_DAYS=2;
-	public static void main(String[] arg)
-	{
-                  //variables
-		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
-	           //computation
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
-	   {
-		int empCheck=(int)(Math.random()*10)%3;
-		switch(empCheck)
-	      {
-		case IS_PART_TIME:
-			empHrs=4;
-			break;
-		case IS_FULL_TIME:
-			empHrs=8;
-			break;
-		default:
-			empHrs=0;
-	       }
-		empWage=empHrs*EMP_RATE_PER_HOUR;
-		totalEmpWage+=empWage;
-		System.out.println("Emp Wage="+empWage);
-	    }
-		System.out.println("Total Emp Wage="+totalEmpWage);	
+public class EmpWageBuilderUC1 {
+	
+	private static final int EMP_RATE_PER_HRS = 20;
+	private static final int NUM_OF_WORKING_DAYS = 20;
+	private static final int MAX_HRS_IN_MONTH = 100;
 
+	public static void main(String[] args) {
+		calDailyEmpWage();
 	}
 
-}
+	/**
+	 * calculate employee daily wages based on type of employee 
+	 */
+	public static void calDailyEmpWage() {
+		int totalWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while(totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+			totalWorkingDays++;
 
+			final int empHrs = getEmpHrs();
+			final int empWage = empHrs*EMP_RATE_PER_HRS;
+			totalEmpHrs+=empHrs;
+			totalWage+=empWage;
+			System.out.println("Emp DAY : "+totalWorkingDays+" wages : "+empWage);
+		}
+		System.out.println("Total emp wage : "+ totalWage);
+	}
+
+	/**
+	 * Get employee hours. 
+	 * @return employee hrs
+	 */
+	public static int getEmpHrs() {
+
+		final int isFullTime = 1;
+		final int isPartTime = 2;
+		int empHrs = 0;
+
+		//get random value
+		final double randomValue = Math.floor(Math.random()*10)%3;
+
+		switch((int)randomValue) {
+
+			case isFullTime:
+				empHrs = 8;
+				System.out.println("Emp is present for full time.");
+				break;
+			case isPartTime:
+				empHrs = 4;
+				System.out.println("Emp is present for part time.");
+				break;
+			default:
+				System.out.println("Emp is absent");
+				break;
+		}
+		return empHrs;
+	}
+}
