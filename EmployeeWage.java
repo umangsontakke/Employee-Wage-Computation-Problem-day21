9241c75a620ed5c81b1b6b6575b6062f0d6f4381
umangsontakke <umangsontakke70@gmail.com>
2020-08-27 21:07:04 +0530

    EmployeeCheck file added to uc1 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,23 @@
-class EmployeeWage
+public class CheckEmpPresentAbsent
 {
-public static void main(String[] args)
-{
-System.out.println("wellcome to employeewage");
+	public static void main(String[] arg)
+	{
+                  //Constants
+		int fullTimeEmp=1;
+	    	int randomCheck=(int)((Math.random()*10)%2);
+		System.out.println(randomCheck);	
+		if(fullTimeEmp==randomCheck)
+	{
+		System.out.println("empoyee is present");
+
+	}
+	else
+	{
+		System.out.println("employee is absent");	
+
+	}
+
 }
+
+
 }
